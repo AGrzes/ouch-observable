7527927207343b90bd3a52d9f0e665e9a8c74db0
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-06-16 23:13:38 +0200

    Index

---
+++ index.js
@@ -0,0 +1,23 @@
+const all = require('./src/all')
+const view = require('./src/view')
+const merge = require('./src/merge')
+const sink = require('./src/sink')
+
+class Ouch {
+  constructor(db){
+    this.db = db
+  }
+  sink(){
+    return sink(this.db)
+  }
+  merge(f){
+    return merge(this.db,f)
+  }
+  all(options){
+    return all(this.db,options)
+  }
+  view(name,options){
+    return all(this.db,name,options)
+  }
+}
+module.exports.Ouch = Ouch
