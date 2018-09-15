b07c9b900e3aeb91ee76a533f1bafefda8a42e96
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-15 22:51:46 +0200

    Changed type of an element emited by changes

--- src/changes.js
+++ src/changes.js
@@ -1,7 +1,7 @@
 const rx = require('rxjs')
 module.exports.changes = (db, options) => rx.Observable.create((observer) => {
   db.changes(options).on('change', (row) => {
-    observer.next(row.doc)
+    observer.next(row)
   })
     .on('complete', () => observer.complete())
     .on('error', (error) => observer.error(error))
