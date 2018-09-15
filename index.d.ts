84e89eb6110f7f07ccb07899d1cbeb17551c67dc
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-15 22:52:23 +0200

    Updated type mapping

--- index.d.ts
+++ index.d.ts
@@ -7,10 +7,10 @@ export type MergeFunction<Model,Content> = (newDoc: Model & Content, oldDoc?: Mo
 export declare class Ouch<Content> {
   constructor(db: PouchDB.Database<Content>)
   public sink<Model>(): OperatorFunction<Model & Content,any>
-  public merge<Model>(mergeFunction: MergeFunction<Model,Content>): OperatorFunction<Model & Content,any>
+  public merge<Model>(mergeFunction: MergeFunction<Model,Content>): OperatorFunction<Model & Content,PouchDB.Core.ExistingDocument<Model & Content>>
   public all<Model>(options?: PouchDB.Core.AllDocsWithKeyOptions | PouchDB.Core.AllDocsWithKeysOptions |
     PouchDB.Core.AllDocsWithinRangeOptions | PouchDB.Core.AllDocsOptions): Observable<Model & Content>
-  public changes<Model>(options?: PouchDB.Core.ChangesOptions): Observable<Model & Content>
+  public changes<Model>(options?: PouchDB.Core.ChangesOptions): Observable<PouchDB.Core.ChangesResponseChange<Model & Content>>
   public view<Model,Result>(name: string, options?: PouchDB.Query.Options<Content,Result>): Observable<Model & Content>
 }
 
